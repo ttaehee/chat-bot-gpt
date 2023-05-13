eb2e31104ebba72fa6397397c312e75f92e85c5a
ttaehee <ttaehee1105@gmail.com>
2023-05-13 14:04:34 +0900

    feat : question entity 구현

---
+++ src/main/java/com/example/bot/domain/Question.java
@@ -0,0 +1,31 @@
+package com.example.bot.domain;
+
+import com.example.bot.global.audit.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Entity
+@Table(name = "question")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Question extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name = "contents", nullable = false)
+    public String contents;
+
+    protected Question(String contents) {
+        this.contents = contents;
+    }
+}
