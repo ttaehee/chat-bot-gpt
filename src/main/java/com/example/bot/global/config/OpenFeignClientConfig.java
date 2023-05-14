b040591062504c5ab95c8e0291f9fc7fa5902f16
ttaehee <ttaehee1105@gmail.com>
2023-05-14 17:45:09 +0900

    chore : Open Feign Client dependency 추가 및 설정

---
+++ src/main/java/com/example/bot/global/config/OpenFeignClientConfig.java
@@ -0,0 +1,13 @@
+package com.example.bot.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class OpenFeignClientConfig {
+
+    @Value("${openai.api-key}")
+    private String apiKey;
+}
